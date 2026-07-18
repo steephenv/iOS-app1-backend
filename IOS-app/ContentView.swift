8416f959c72f032a8a991c0e66426f66c5046efd
Steephen Varghese <steephenvrs3@gmail.com>
2026-07-18 11:28:21 +0400

    Initial Commit

---
+++ IOS-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS-app
+//
+//  Created by Steephen Varghese on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS-app/IOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_appApp.swift
+//  IOS-app
+//
+//  Created by Steephen Varghese on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
